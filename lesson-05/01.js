2173e71725627bc03374c24f644d6fec15167cd9
Kim Vyacheslav <kimv.oppa@gmail.com>
2026-05-21 14:37:46 +0500

    done 'hw'

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -11,9 +11,15 @@
 */
 
 const game = {
-  resources: {
-    gold: 250,
-    lumber: 100,
-  },
-  addResource() {}
+	resources: {
+		gold: 250,
+		lumber: 100,
+	},
+	addResource(resource, amount) {
+		if (!(resource in game.resources)) {
+			console.log("Invalid resource")
+		} else {
+			game.resources[resource] += amount
+		}
+	}
 }
--- lesson-05/02.js
+++ lesson-05/02.js
@@ -30,9 +30,16 @@ console.log(gallery)
 */
 
 const gallery = {
-  'Mona Lisa': 'Leonardo da Vinci',
-  'Starry Night': 'Vincent van Gogh',
-  'The Scream': 'Edvard Munch',
+	'Mona Lisa': 'Leonardo da Vinci',
+	'Starry Night': 'Vincent van Gogh',
+	'The Scream': 'Edvard Munch',
 }
 
-function updateGallery() {}
+function updateGallery(obj, key, value) {
+	obj[key] = value
+}
+
+updateGallery(gallery, 'Mona Lisa', 'Leonardo da Vinci, 1503-1506')
+updateGallery(gallery, 'The Persistence of Memory', 'Salvador Dali')
+
+console.log(gallery)
