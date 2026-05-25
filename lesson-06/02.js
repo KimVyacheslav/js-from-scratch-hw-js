4c9f1955f314035cab7c80cc0772e09d8d09cca2
Kim Vyacheslav <kimv.oppa@gmail.com>
2026-05-25 18:31:10 +0500

    done 'hw'

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,13 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+	let newArr = []
+	for (let i = 0; i < array.length; i++) {
+		const result = callback(array[i], i)
+		newArr.push(result)
+	}
+	return newArr
+}
+
+const numbers = [1, 2, 3, 4, 5]
