7f324a30c0db6acb5a3d10da19f0f17c3aa02171
Kim Vyacheslav <kimv.oppa@gmail.com>
2026-06-08 13:57:14 +0500

    done 'hw6'

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -13,7 +13,31 @@
 */
 
 const WEB_TECH_IMAGES = [
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
-  'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
+	'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
+	'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
+	'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
 ]
+
+const prevBtn = document.getElementById("prev-button")
+const nextBtn = document.getElementById("next-button")
+const webTech = document.getElementById("web-tech-image")
+let numberList = 0;
+nextBtn.addEventListener("click", function () {
+	if (numberList < WEB_TECH_IMAGES.length - 1) {
+		numberList += 1
+	} else {
+		numberList = 0
+	}
+	webTech.src = WEB_TECH_IMAGES[numberList]
+})
+
+prevBtn.addEventListener("click", function () {
+	if (numberList === 0) {
+		numberList = WEB_TECH_IMAGES.length - 1
+	} else {
+		numberList -= 1
+	}
+	webTech.src = WEB_TECH_IMAGES[numberList]
+})
+
+
