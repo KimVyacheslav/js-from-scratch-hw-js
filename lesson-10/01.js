ea14a55a586f713043140e6a4b77a6dd89d358e2
Kim Vyacheslav <kimv.oppa@gmail.com>
2026-06-28 15:00:56 +0500

    done hw "10"

--- lesson-10/01.js
+++ lesson-10/01.js
@@ -22,79 +22,92 @@
 */
 
 const model = {
-  movies: [],
-  addMovie(title, description) {
-    const id = Math.random()
-    const newMovie = { id, title, description }
-    this.movies.push(newMovie)
-    view.renderMovies(this.movies)
-  },
-  // your code
+	movies: [],
+	addMovie(title, description) {
+		const id = Math.random()
+		const newMovie = {id, title, description}
+		this.movies.push(newMovie)
+		view.renderMovies(this.movies)
+	},
+	deleteMovie(movieId) {
+		const movieFilter = this.movies.filter(m => m.id !== movieId)
+		this.movies = movieFilter
+		view.renderMovies(movieFilter)
+	}
 }
 
 const view = {
-  init() {
-    this.renderMovies(model.movies)
-
-    const form = document.querySelector('.form')
-    const inputTitle = document.querySelector('.input-title')
-    const inputDescription = document.querySelector('.input-description')
-
-    form.addEventListener('submit', function (event) {
-      event.preventDefault()
-      const title = inputTitle.value
-      const description = inputDescription.value
-      controller.addMovie(title, description)
-
-      inputTitle.value = ''
-      inputDescription.value = ''
-    })
-
-    // your code
-  },
-  renderMovies(movies) {
-    const list = document.querySelector('.list')
-    let moviesHTML = ''
-
-    for (const movie of movies) {
-      moviesHTML += `
+	init() {
+		this.renderMovies(model.movies)
+		
+		const form = document.querySelector('.form')
+		const inputTitle = document.querySelector('.input-title')
+		const inputDescription = document.querySelector('.input-description')
+		
+		form.addEventListener('submit', function (event) {
+			event.preventDefault()
+			const title = inputTitle.value
+			const description = inputDescription.value
+			controller.addMovie(title, description)
+			
+			inputTitle.value = ''
+			inputDescription.value = ''
+		})
+		
+		const list = document.querySelector(".list")
+		list.addEventListener("click", function (event) {
+			if (event.target.classList.contains("delete-button")) {
+				controller.deleteMovie(event.target.parentElement.id)
+			}
+		})
+		
+	},
+	renderMovies(movies) {
+		const list = document.querySelector('.list')
+		let moviesHTML = ''
+		
+		for (const movie of movies) {
+			moviesHTML += `
         <li id="${movie.id}" class="movie">
           <b class="movie-title">${movie.title}</b>
           <p class="movie-description">${movie.description}</p>
           <button class="delete-button" type="button">Удалить 🗑</button>
         </li>
       `
-    }
-
-    list.innerHTML = moviesHTML
-  },
-  displayMessage(message, isError = false) {
-    const messageBox = document.querySelector('.message-box')
-    messageBox.textContent = message
-    if (isError) {
-      messageBox.classList.remove('success')
-      messageBox.classList.add('error')
-    } else {
-      messageBox.classList.remove('error')
-      messageBox.classList.add('success')
-    }
-  },
+		}
+		
+		list.innerHTML = moviesHTML
+	},
+	displayMessage(message, isError = false) {
+		const messageBox = document.querySelector('.message-box')
+		messageBox.textContent = message
+		if (isError) {
+			messageBox.classList.remove('success')
+			messageBox.classList.add('error')
+		} else {
+			messageBox.classList.remove('error')
+			messageBox.classList.add('success')
+		}
+	},
 }
 
 const controller = {
-  addMovie(title, description) {
-    if (title.trim() !== '' && description.trim() !== '') {
-      model.addMovie(title, description)
-      view.displayMessage('Фильм добавлен успешно!')
-    } else {
-      view.displayMessage('Заполните все поля!', true)
-    }
-  },
-  // your code
+	addMovie(title, description) {
+		if (title.trim() !== '' && description.trim() !== '') {
+			model.addMovie(title, description)
+			view.displayMessage('Фильм добавлен успешно!')
+		} else {
+			view.displayMessage('Заполните все поля!', true)
+		}
+	},
+	deleteMovie(movieId) {
+		model.deleteMovie(movieId)
+		view.displayMessage("Фильм успешно удалён!")
+	}
 }
 
 function init() {
-  view.init()
+	view.init()
 }
 
 document.addEventListener('DOMContentLoaded', init)
